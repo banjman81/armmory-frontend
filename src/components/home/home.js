beaa8f4c3ded8530d0e8df6fe311df4a9f5808ee
[banjman81] <benjamin.man97@gmail.com>
2021-12-06 16:06:41 -0600

    inital commit

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export class Home extends Component {
+    state = {
+        isLoading : false,
+        gameArray : []
+    }
+
+    async componentDidMount(){
+        
+    }
+}
