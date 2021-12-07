a0a2e1aacbc8aeb56595fe5aeb2f79ac3e3ede02
[banjman81] <benjamin.man97@gmail.com>
2021-12-07 17:25:13 -0600

    form

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,42 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
+import './user.css'
 
 function SignUp(){
 
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmaPassword] = useState('')
+    const [adminPass, setAdminPass] = useState('')
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        console.log(firstName)
+        console.log(lastName)
+        console.log(username)
+        console.log(email)
+        console.log(password)
+        console.log(confirmPassword)
+        console.log(adminPass)
+    }
+
     return(
-        <div className="App">
-            <form>
-                <table>
+        <div>
+            <form onSubmit={(e)=> handleSubmit(e)}>
+                <table className="signup-table">
                     <tbody>
                         <tr>
-                            <td>First Name</td>
-                            <td><input type="text" /></td>
+                            <td><input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}placeholder="First Name"/></td>
+                            <td><input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last Name"/></td>
                         </tr>
                         <tr>
-                            <td>Last Name</td>
-                            <td><input type="text" /></td>
+                            <td colSpan="2"><input className="full-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/></td>
                         </tr>
                         <tr>
-                            <td>Username</td>
-                            <td><input type="text" /></td>
+                            <td colSpan="2"><input className="full-input" type="text" value={email} onChange={(e) => setEmail(e.target.value)}/></td>
                         </tr>
                         <tr>
-                            <td>Email</td>
-                            <td><input type="text" /></td>
+                            <td colSpan="2"><input className="full-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/></td>
                         </tr>
                         <tr>
-                            <td>Password</td>
-                            <td><input type="password" /></td>
+                            <td colSpan="2"><input className="full-input" type="password" value={confirmPassword} onChange={(e) => setConfirmaPassword(e.target.value)}/></td>
                         </tr>
                         <tr>
-                            <td>Confirm Password</td>
-                            <td><input type="password" /></td>
+                            <td colSpan="2"><input className="full-input" type="password" value={adminPass} onChange={(e) => setAdminPass(e.target.value)}/></td>
                         </tr>
                         <tr>
-                            <td>Admin Pass</td>
-                            <td><input type="password" /></td>
+                            <td colSpan="2"><button>Submit</button></td>
                         </tr>
                     </tbody>
                 </table>
+                
             </form>
         </div>
     )
