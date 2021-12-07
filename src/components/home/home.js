6441b3083c326ed4a67ebf19ef0c006676b65373
[banjman81] <benjamin.man97@gmail.com>
2021-12-07 10:04:40 -0600

    connect home to api

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, {Component} from "react";
+import './home.css'
 
 export class Home extends Component {
     state = {
@@ -8,12 +10,39 @@ export class Home extends Component {
 
     async componentDidMount(){
 
+        this.setState({
+            isLoading : true
+        })
+
+        try{
+
+            let payload = await axios.get('https://www.mmobomb.com/api1/games')
+            console.log(payload)
+            this.setState({
+                gameArray : payload.data,
+                isLoading : false
+            })
+
+        }catch(err){
+            console.log(err)
+        }
+
     }
 
     render(){
         return(
-            <div>
-                <h1>Home</h1>
+            <div className="home-container">
+                {this.state.isLoading ? ("LOading...") :(
+                    this.state.gameArray.map((item) => {
+                        return(
+                            <div key={item.id} className="item-container">
+                                <h1>{item.title}</h1>
+                                <img src={item.thumbnail} alt="img" />
+                                <p>{item.short_description}</p>
+                            </div>
+                        )
+                    })
+                )}
             </div>
         )
     }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,10 +6,14 @@ function Nav(){
     return(
         <div className="nav-container">
             <ul className="nav-bar">
-                <li><Link to="/"><h1>HOME</h1></Link></li>
-                <li><Link to="/signup">Sign up</Link></li>
-                <li><Link to="/signin">Sign in</Link></li>
+                <li><Link className="nav-link" to="/"><h1>HOME</h1></Link></li>
+                <li><Link className="nav-link" to="/signup">Sign up</Link></li>
+                <li><Link className="nav-link" to="/signin">Sign in</Link></li>
             </ul>
+            <div className="search-bar">
+                <input type="text" name="" id="" />
+                <button>Search</button>
+            </div>
         </div>
     )
 }
